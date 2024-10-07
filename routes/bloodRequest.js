0be56e02cecb3bbd3dd45c88126530335b8ea03e
robinanbu <robinanbu23@gmail.com>
2024-10-07 12:24:35 +0530

    Employee Dashboard Working

--- routes/bloodRequest.js
+++ routes/bloodRequest.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 const Patient = require('../models/Patient');
 
-router.get('/', async ( req, res) => {
-    try{
-        res.send("This is working!!!! wow");
-    }catch (error){
-        res.status(404).json({error: "error"})
-    }  
-})
+// router.get('/', async ( req, res) => {
+//     try{
+//         res.send("This is working!!!! wow");
+//     }catch (error){
+//         res.status(404).json({error: "error"})
+//     }  
+// })
 // POST route to handle blood requests
 router.post('/', async (req, res) => {
     try {
@@ -29,6 +29,21 @@ router.post('/', async (req, res) => {
         console.error('Error submitting blood request:', error);
         res.status(500).json({ error: 'Server error. Please try again later.' });
     }
+});
+
+router.get('/',async (req,res) => {
+    try {
+        const patients = await Patient.find({});
+        return res.status(200).json({
+            patients
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({
+            message: error.message,
+        });
+    }
+    
 });
 
 module.exports = router;
